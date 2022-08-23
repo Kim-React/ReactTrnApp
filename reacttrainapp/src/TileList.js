b8f87f36005a4e69402428a5c49d5afc665a5b07
uitricks <learn.uitricks@gmail.com>
2022-08-23 21:22:32 +0530

    5listoftiles

--- reacttrainapp/src/TileList.js
+++ reacttrainapp/src/TileList.js
@@ -3,21 +3,29 @@ import React from 'react';
 
 class TileList extends React.Component{
     
-    state  = {
-        tile:{name:"somany",model:"nit-01",price:200,rating:2,"status":1}
-    }
+    state={
+        tiles:[
+          {"name":"somany","model":"nit-01","price":200,"rating":2,"image":"tile1.jpg","status":1,"Id":1},
+          {"name":"johnson","model":"nit-02","price":100,"rating":4,"image":"tile2.jpg","status":0,"Id":2},
+          {"name":"hsil","model":"nit-03","price":150,"rating":5,"image":"tile3.jpg","status":1,"Id":3},
+          {"name":"clayhaus","model":"nit-04","price":350,"rating":3,"image":"tile4.jpg","status":1,"Id":4},
+        ]
+      }
+      
     render(){
-        return (<div> <div>
-            {this.state.tile.name}</div>
-            <div>
-            {this.state.tile.model}
-            </div>
-            <div>
-            {this.state.tile.price}
-            </div>
-            <div>
-            {this.state.tile.status}
-            </div>
+        return (<div> 
+            {this.state.tiles.map((tile,index)=> {
+           return (
+              <tr key={index}>
+              <td>{tile.name}</td>  
+              <td>{tile.model}</td>
+              <td>{tile.price}</td>
+              <td>{tile.rating}</td>
+              <td>{tile.status}</td>
+              </tr>
+            );
+          })}
+
        </div>);
     }
 

17bc88b0e2d8d8d087f10c0e26bf75118350fde0
uitricks <learn.uitricks@gmail.com>
2022-08-24 13:42:19 +0530

    19hookincrementwithprevstate

--- reacttrainapp/src/Components/HookCounterTwo.js
+++ reacttrainapp/src/Components/HookCounterTwo.js
@@ -3,13 +3,25 @@ import React ,{useState}from 'react'
 export default function HookCounterTwo() {
     const initialState = 0;
     const [count,setCount] = useState(initialState);
+
+    function increment(){
+        setCount(prevState => prevState + 1 )
+    }
+
+    function incrementByFive(){
+        increment();
+        increment();
+        increment();
+        increment();
+    }
   
   return (
     <div>
         <h1>counter value :{count}</h1>
       <button onClick={()=> setCount(initialState)}>reset </button>
-      <button onClick={()=> setCount(count+1)}>increment </button>
+      <button onClick={()=> increment()}>increment </button>
       <button onClick={()=> setCount(count-1)}>decrement </button>
+      <button onClick={()=> incrementByFive()}>increment by 5 </button>
     </div>
   )
 }

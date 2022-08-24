8d80cd505cfe6f05e3284a685e3635d019047ed1
uitricks <learn.uitricks@gmail.com>
2022-08-24 15:02:06 +0530

    20userObjectWithFirstNameLastNameUpdateStage2

--- reacttrainapp/src/Components/User.js
+++ reacttrainapp/src/Components/User.js
@@ -2,9 +2,26 @@ import React, { useState } from 'react'
 
 export default function User() {
     const [name,setName] = useState({firstName:'Elon', lastName:'Musk'})
+
+    function updateFirstName(e){
+        const nameValue = e.target.value;
+        setName({firstName:nameValue});
+    }
+    function updateLastName(e){
+        const nameValue = e.target.value;
+        setName({lastName:nameValue});
+    }
   return (
+    <>
     <div>
         <h1>{name.firstName}</h1> : <h1>{name.lastName}</h1>
     </div>
+    <div>
+        <form>
+        <div> first name : <input type="text"  value={name.firstName}  onChange={updateFirstName} />    </div>
+            <div> lastName  : <input type="text" value={name.lastName} onChange={updateLastName}/>    </div>
+        </form>
+    </div>
+    </>
   )
 }

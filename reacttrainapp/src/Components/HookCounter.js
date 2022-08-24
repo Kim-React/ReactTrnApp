f7e6a65b238c3b25172ab3b4ea0f34603317d885
uitricks <learn.uitricks@gmail.com>
2022-08-24 13:13:06 +0530

    17HookCounterWithFatArrowFn

--- reacttrainapp/src/Components/HookCounter.js
+++ reacttrainapp/src/Components/HookCounter.js
@@ -2,13 +2,11 @@ import React ,{useState}from 'react'
 
 export default function HookCounter() {
     const [count,setCount] = useState(0);
-    function updateCounter(){
-        setCount(count+1);
-    }
+  
   return (
     <div>
         <h1>counter value :{count}</h1>
-      <button onClick={updateCounter}>increment counter</button>
+      <button onClick={()=> setCount(count+1)}>increment counter</button>
     </div>
   )
 }
